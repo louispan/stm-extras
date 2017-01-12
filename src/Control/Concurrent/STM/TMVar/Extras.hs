4e7db1a0e7956e2045c853b2ae94fbc72ab57b03
Louis Pan <louis@pan.me>
2017-01-12 20:29:51 +1100

    Added wait methods

--- src/Control/Concurrent/STM/TMVar/Extras.hs
+++ src/Control/Concurrent/STM/TMVar/Extras.hs
@@ -3,7 +3,17 @@ module Control.Concurrent.STM.TMVar.Extras where
 import Control.Concurrent.STM
 import Control.Concurrent.STM.TMVar
 
--- | forces a swap of TMVar irreguardless if it previously contained a value or not.
+-- | Non-blocking swap of TMVar irreguardless if it previously contained a value or not.
 -- Returns what was in the TMVar (if exists)
 forceSwapTMVar :: TMVar a -> a -> STM (Maybe a)
 forceSwapTMVar v a = (Just <$> swapTMVar v a) `orElse` (const Nothing <$> putTMVar v a)
+
+-- | Block until TMVar is empty.
+-- The argument a is used to try to put into TMVar, but is taken out again in the same
+-- transaction
+waitTillEmptyTMVar :: TMVar a -> a -> STM ()
+waitTillEmptyTMVar v a = putTMVar v a >> takeTMVar v >> pure ()
+
+-- | Block until TMVar is full.
+waitTillFullTMVar :: TMVar a -> STM ()
+waitTillFullTMVar v = takeTMVar v >>= putTMVar v
