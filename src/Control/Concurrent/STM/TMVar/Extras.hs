be9a98db59972c0f5cc8f2a1173bb157c9b886cd
Louis Pan <louis@lory.local>
2017-07-07 21:59:58 +1000

    Removed inline pragmas until I have actual benchmarks

--- src/Control/Concurrent/STM/TMVar/Extras.hs
+++ src/Control/Concurrent/STM/TMVar/Extras.hs
@@ -1,22 +1,18 @@
 module Control.Concurrent.STM.TMVar.Extras where
 
 import Control.Concurrent.STM
-import Control.Concurrent.STM.TMVar
 
 -- | Non-blocking swap of TMVar irreguardless if it previously contained a value or not.
 -- Returns what was in the TMVar (if exists)
 forceSwapTMVar :: TMVar a -> a -> STM (Maybe a)
 forceSwapTMVar v a = (Just <$> swapTMVar v a) `orElse` (const Nothing <$> putTMVar v a)
-{-# INLINABLE forceSwapTMVar #-}
 
 -- | Block until TMVar is empty.
 -- The argument a is used to try to put into TMVar, but is taken out again in the same
 -- transaction
 waitTillEmptyTMVar :: TMVar a -> a -> STM ()
 waitTillEmptyTMVar v a = putTMVar v a >> takeTMVar v >> pure ()
-{-# INLINABLE waitTillEmptyTMVar #-}
 
 -- | Block until TMVar is full.
 waitTillFullTMVar :: TMVar a -> STM ()
 waitTillFullTMVar v = takeTMVar v >>= putTMVar v
-{-# INLINABLE waitTillFullTMVar #-}
